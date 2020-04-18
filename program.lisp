0fd68d91c9aaa3dd5ca94fbaf6e362882390cfc7
alok <work.deka.alok@gmail.com>
2020-04-19 03:41:10 +0530

    updated

--- program.lisp
+++ program.lisp
@@ -3,10 +3,10 @@
     (read))
 (defun hello-you 
     (*name*)
-    (format t "Hello ~s!~%" *name*)
-    (format t "Hello ~a!~%" *name*)
-    (format t "Hello ~10a!~%" *name*)
-    (format t "Hello ~10@a!~%" *name*)
+    (format t "Hello ~a!~%" *name*) ;; ~a shows the value
+    (format t "Hello ~s!~%" *name*) ;; ~s shows quotes around the value
+    (format t "Hello ~10a!~%" *name*) ;; ~10a adds 10 spaces for the value with extra space to the right
+    (format t "Hello ~10@a!~%" *name*) ;; ~10@a adds 10 spaces for the value with extra space to the left
 )
 (setq *print-case* :capitalize)
 (hello-you *name*)
